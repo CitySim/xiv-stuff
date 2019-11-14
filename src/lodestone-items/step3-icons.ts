7e2b489b38a5050a167f755f9e4fd88441d3fcf3
Sven Tatter <sven31894@gmail.com>
2019-11-14 20:29:01 +0100

    fix memory leak, and other stuff

--- src/lodestone-items/step3-icons.ts
+++ src/lodestone-items/step3-icons.ts
@@ -3,11 +3,11 @@ import { cacheString, sleep, getDom } from "../util";
 import { Item } from "./types";
 import * as domUtils from "domutils";
 import { writeFileSync } from "fs";
+import { Aigle } from "aigle";
 
 export async function main(items: Item[]) {
-	for (let i = 0; i < items.length; i++) {
-		let item = items[i];
-
+	let done = 0;
+	await Aigle.eachLimit(items, 5, async(item) => {
 		let html = await cacheString(`lodestone-item-${item.lodestoneId}.html`, async () => {
 			let res = await got(`https://eu.finalfantasyxiv.com/lodestone/playguide/db/item/${item.lodestoneId}/`);
 			return res.body;
@@ -22,16 +22,22 @@ export async function main(items: Item[]) {
 		for (let icon of icons) {
 			let className = domUtils.getAttributeValue(icon, "class") || "";
 			if (className.includes("sys_nq_element")) {
-				item.nq = domUtils.getAttributeValue(icon, "src");
+				item.nq = unrefString(domUtils.getAttributeValue(icon, "src"));
 			} else if (className.includes("sys_hq_element")) {
-				item.hq = domUtils.getAttributeValue(icon, "src");
+				item.hq = unrefString(domUtils.getAttributeValue(icon, "src"));
 			}
 		}
 
-		console.log(`${i + 1}/${items.length} ${item.nq ? "NQ" : "  "} ${item.hq ? "HQ" : "  "} - ${item.name}`)
+		console.log(`${++done}/${items.length} ${item.nq ? "NQ" : "  "} ${item.hq ? "HQ" : "  "} - ${item.name}`)
 
-		writeFileSync("items.json", JSON.stringify(items, null, 2));
-	}
+		// writeFileSync("items.json", JSON.stringify(items, null, 2));
+	});
 
 	return items;
+}
+
+// this is due a fancy v8 does
+// https://bugs.chromium.org/p/v8/issues/detail?id=2869
+function unrefString(input: string): string {
+	return (" " + input).substr(1);
 }
